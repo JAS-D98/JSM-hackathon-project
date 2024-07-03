7ad99a6d35fda54a1a1371091d8332e03f98956b
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-07-04 00:06:39 +0300

    add articles page

---
+++ src/components/articleCard/ArticleCard.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// export default function 
+// () {
+//   return (
+//     <div>
+//         <h1><span className='text-primaryBlueColor'>Title:</span>{title}</h1>
+//         <p>{article}</p>
+//         <p><span className='text-primaryBlueColor'>Writer:{writer}</span></p>
+//     </div>
+//   )
+// }
---
+++ src/data/Articles.js
@@ -0,0 +1,4 @@
+export const articles=[
+    {title:"Enhancing student learning", article:"Schools can improve student learning through personalized education, tailored to individual needs, and collaborative learning, promoting critical thinking and teamwork. Integrating technology, such as interactive whiteboards and educational software, further enhances engagement and effectiveness, creating a dynamic and supportive learning environment.", writer:"Mary Macharia"},
+    {title:"Fostering creativity in schools", article:"Encouraging creativity in schools is essential for developing innovative thinkers. Schools can achieve this by incorporating arts and music programs, offering problem-solving activities, and providing opportunities for students to explore their interests. Creating a supportive environment where students feel safe to express their ideas can significantly enhance their creative abilities.", writer:"Jared Ouma"},
+]
