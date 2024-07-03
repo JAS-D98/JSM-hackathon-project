6123949ed29753109ba8aa011b3deb375e360bd2
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-07-03 13:39:58 +0300

    remove button icon

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Button({
   title,
-  icon,
   marginTop,
   backgroundColor,
   border,
@@ -13,7 +12,7 @@ export default function Button({
     className={`flex items-center justify-center px-2 py-1 md:px-3 md:py-2 max-w-[200px] shadow-md mt-${marginTop ? marginTop : `6`} text-white bg-${backgroundColor ? backgroundColor : "bg-primaryBlueColor"} gap-2 rounded-md capitalize hover:opacity-70 ${border ? border : ""}`}
     onClick={funcWord}
     >
-      {title} {icon && <img src={icon} width={20} className="rotate-45 mt-1" />}
+      {title}
     </button>
   );
 }
