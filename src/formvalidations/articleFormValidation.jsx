1d071f0cec347015187538e70f2a4b3ff8b12f60
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-12-21 01:25:15 +0300

    fix bug on articleFormValidation causing deployment issues

--- src/formvalidations/articleFormValidation.jsx
+++ src/formvalidations/articleFormValidation.jsx
@@ -4,4 +4,4 @@ export const articleFormValidation = Yup.object({
   name: Yup.string().min(3).required("Please Enter Name"),
   title: Yup.string().min(3).required("Please Enter Title"),
   article: Yup.string().required("Please Enter Your Article"),
-});
+})
